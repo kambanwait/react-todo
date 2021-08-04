2feee0cf1ad44f937cfb68bae997e7f310b7cae8
Scripted Pixels <kam@scriptedpixels.co.uk>
2021-08-04 15:09:47 +0100

    imports new modal and background components and import userState from react
    Function to set modal visibility to true
    Function to close modal
    Function to handle button events in modal to then pass to parent app.js with todo to be removed from the todo's array

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,11 +1,46 @@
+import { useState } from 'react'
+
+import Modal from './Modal';
+import Backdrop from './Backdrop';
+
 function Todo (props) {
+
+  const [ isModalOpen, setModalIsOpen] = useState(false)
+
+  function deleteHandler () {
+    setModalIsOpen(true)
+  }
+
+  function closeModal () {
+    setModalIsOpen(false)
+  }
+
+  function confirmTodoDelete () {
+    props.onDelete(props.title)
+    closeModal()
+  }
 
   return (
     <div className='card'>
-      <h2>{props.title}</h2>
+
+      <h3>{props.title}</h3>
+
       <div className='actions'>
-        <button className='btn'>Delete</button>
+        <button onClick={deleteHandler} className='btn'>Delete</button>
       </div>
+
+      {/* Modal */}
+      { isModalOpen &&
+        <Modal
+          todoTitle={props.title}
+          onCancel={closeModal}
+          onConfirmDeleteClick={confirmTodoDelete}
+        />
+      }
+
+      {/* Backdrop */}
+      { isModalOpen && <Backdrop onClick={closeModal} /> }
+
     </div>
   )
 }
