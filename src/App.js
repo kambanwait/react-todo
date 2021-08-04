cca33ef62d9a4a9412f34192d056cd2a1b50839a
Scripted Pixels <kam@scriptedpixels.co.uk>
2021-08-04 15:11:36 +0100

    Adds functionality for Todo
    - useState to add an initial empty array of todo's on load
    - Function to add todo
    - Function to remove todo
    - Import todo form
    - Map over Todo's and use Todo component to pass in title and functions to delete todo's

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
+import { useState } from 'react';
+
 import Todo from './components/Todo'
+import TodoForm from './components/TodoForm'
 
 function App() {
+
+  const [todos, setTodos] = useState([])
+
+  function AddTodo (todoTitle) {
+    setTodos([...todos, todoTitle])
+  }
+
+  function removeTodo (todoTitle) {
+    todos.splice( todos.indexOf(todoTitle), 1)
+    setTodos([...todos])
+  }
+
   return (
     <div>
-
       <h1>My Todos</h1>
 
-      <Todo title="A new Todo using Props" />
-      <Todo title="Another Todo" />
-      <Todo title="Do stuff" />
+      <TodoForm saveTodo={AddTodo} />
 
+      {todos.map((todo, index) => (
+        <Todo
+          key={index.toString()}
+          title={todo}
+          onDelete={removeTodo}
+        />
+      ))}
     </div>
   );
 }
