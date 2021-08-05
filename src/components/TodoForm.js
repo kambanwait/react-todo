7ddaf4cfb8dd60dc4b01cfa90dd202970a6faa7e
Scripted Pixels <kam@scriptedpixels.co.uk>
2021-08-05 22:18:53 +0100

    Adding comments

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,18 +1,32 @@
 import { useState } from 'react'
 
+/**
+ * To do form holds and handles the input form at the top of the app where users can add a to do
+ * @param {Object} props contains function from praent app.js to call when want to save a to do (props.saveTodo).
+ * pass in the value from the input field
+ * @returns {JSX.Element}
+ * @constructor
+ */
 function TodoForm (props) {
 
   const [value, setValue] = useState('')
 
   function handleFormSubmit (event) {
     event.preventDefault()
+
+    // check to see if the input is empty, if so, return immediately
+    // Todo: highlight error to user when no text is entered, useState to control error state?
+    if (value.trim() === "") return false
+
+    // send the new value to the props function in app.js
     props.saveTodo(value)
 
     // clear the input field by setting value to empty string
     setValue('')
   }
 
-  function handleOnInputchange (event) {
+  // each time a user types into the input field, we fire an event to update the value in our state
+  function handleOnInputChange (event) {
     setValue(event.target.value)
   }
 
